0997db4e0075a435ed9591c7c7579366e4955534
Willian Proni <wifepro@hotmail.com>
2022-04-04 23:53:12 -0300

    Finish MicroService Passeger

--- Model/Address.cs
+++ Model/Address.cs
@@ -3,22 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
 namespace Model
 {
     public class Address
     {
-        [BsonId]
-        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
-        public string Id { get; set; }
-        public string District { get; set; }
-        public string City { get; set; }
-        public string Country { get; set; }
+        public string Id { get; set; } = ObjectId.GenerateNewId().ToString();
         public string Cep { get; set; }
-        public string Street { get; set; }
+        public string City { get; set; }
         public string State { get; set; }
+        public string District { get; set; }
+        public string Street { get; set; }
         public int Number { get; set; }
         public string Complement { get; set; }
+        public string Country { get; set; }
     }
 }
---
+++ Model/Airport.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    internal class Airport
+    {
+        public string Id { get; set; }
+        public int MyProperty { get; set; }
+    }
+}
