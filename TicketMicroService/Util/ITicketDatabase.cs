de1f0fafc810356b1470ec0d0004b2e4a106e8d8
Willian Proni <wifepro@hotmail.com>
2022-04-05 11:09:51 -0300

    Finish MicroService Ticket

---
+++ TicketMicroService/Util/ITicketDatabase.cs
@@ -0,0 +1,9 @@
+﻿namespace TicketMicroService.Util
+{
+    public interface ITicketDatabase
+    {
+        string TicketCollectionName { get; set; }
+        string ConnectionString { get; set; }
+        string DatabaseName { get; set; }
+    }
+}
---
+++ TicketMicroService/Util/TicketDatabase.cs
@@ -0,0 +1,9 @@
+﻿namespace TicketMicroService.Util
+{
+    public class TicketDatabase : ITicketDatabase
+    {
+        public string TicketCollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
