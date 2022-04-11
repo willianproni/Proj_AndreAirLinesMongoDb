71583175ea34403e906bfe9cc1277076a4efb446
Willian Proni <wifepro@hotmail.com>
2022-04-11 12:01:58 -0300

    MicroService TokenJWS

---
+++ AuthenticationJWS/Controllers/LoginJWSController.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using AuthenticationJWS.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Model;
+using Services;
+
+namespace AuthenticationJWS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginJWSController : ControllerBase
+    {
+        [HttpPost]
+        [Route("login")]
+        public async Task<ActionResult<dynamic>> AuthenticateAsync(User model)
+        {
+            var user = await ServiceSeachApiExisting.SeachUserInApiByLoginUser(model.LoginUser);
+
+            if (user == null)
+                return NotFound("User or password invalid, try again");
+
+            var token = TokenService.GenerateToken(user);
+
+            user.Password = "";
+
+            return new
+            {
+                user = user,
+                token = token
+            };
+        }
+    }
+}
---
+++ AuthenticationJWS/Services/TokenService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using AuthenticationJWS.Config;
+using Microsoft.IdentityModel.Tokens;
+using Model;
+
+namespace AuthenticationJWS.Services
+{
+    public class TokenService
+    {
+        public static string GenerateToken(User user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler(); //Gerar o Token
+            var key = Encoding.ASCII.GetBytes(Settings.Secret); //Enconding, trasforma a chave em um Array de Bytes
+            var tokenDescriptor = new SecurityTokenDescriptor //Descreve as informações principais para o token funcionar
+            {
+                Subject = new ClaimsIdentity(new Claim[] //Informações relacionadas ao Usuário
+                {
+                    new Claim(ClaimTypes.Name, user.Login.ToString()), //User.Identity.Login
+                    new Claim(ClaimTypes.Role, user.Function.Access.Id.ToString()), //User.Identity.Function.Access.Id
+                }),
+                Expires = DateTime.UtcNow.AddHours(2), //Quanto tempo o Token demora para expirar
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor); //Criando o Token
+            return tokenHandler.WriteToken(token); //Retonrnado o Token
+        }
+    }
+}
