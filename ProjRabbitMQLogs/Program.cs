06e4f27e25b7fafec595d6917a40c7d08300124f
Willian Proni <wifepro@hotmail.com>
2022-04-13 11:54:19 -0300

    Test Rabbit

---
+++ ProjRabbitMQLogs/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using RabbitMQ.Client;
+
+namespace ProjRabbitMQLogs
+{
+    internal class Program
+    {
+        private const string QUEUE_NAME = "messagelogs";
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+
+                }
+            }
+        }
+    }
+}
---
+++ ProjRabbitMQLogs/Service/SenderMongoDBservice.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace ProjRabbitMQLogs.Service
+{
+    public class SenderMongoDBservice
+    {
+        private readonly HttpClient client = new HttpClient();
+
+        public static async Task Add(Log log)
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+
+                if (client.BaseAddress == null)
+                    await client.PostAsJsonAsync("https://localhost:44366/api/Log", log);
+
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("Message :{0} ", e.Message);
+            }
+        }
+    }
+}
